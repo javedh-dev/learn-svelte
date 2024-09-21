5ec9734d5890a765872f7e899e691d02b565453f
Javed Hussain <javedh.dev@gmail.com>
2024-09-21 08:56:17 +0000

    Navigating Programitically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,23 @@
+<script>
+	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
+
+	const handleClick = () => {
+		console.log('Placing the order...');
+		goto('/products');
+	};
+
+	beforeNavigate((navigation) => {
+		console.log('before navigate', navigation);
+	});
+
+	afterNavigate((navigation) => {
+		console.log('after navigate', navigation);
+	});
+</script>
+
 <h1>Welcome Home!</h1>
 
 <a href="/blog">Blog</a>
 <a href="/products">Products</a>
+
+<button on:click={handleClick}>Place Order</button>
