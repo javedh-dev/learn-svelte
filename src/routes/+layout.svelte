c3c9317de8d223c1a683905c6cfda64552e399d7
Javed Hussain <javedh.dev@gmail.com>
2024-09-21 09:25:17 +0000

    Route Layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<header class="layout-header">Header</header>
+<slot />
+<footer class="layout-footer">Footer</footer>
+
+<style>
+	.layout-header,
+	.layout-footer {
+		background: #f8f9fa;
+		padding: 30px;
+		text-align: center;
+	}
+</style>
