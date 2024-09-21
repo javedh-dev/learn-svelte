b5a26d929539398bb2a5b22625bc3bb910990911
Javed Hussain <javedh.dev@gmail.com>
2024-09-21 05:13:26 +0000

    Catch All Routes

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+	const slug = $page.params.slug;
+
+	const slugArr = slug.split('/');
+</script>
+
+<h1>
+	Documentation for
+	{#if slugArr.length === 1}
+		Feature : {slugArr[0]}
+	{:else}
+		Feature : {slugArr[0]}, Concept : {slugArr[1]}
+	{/if}
+</h1>
