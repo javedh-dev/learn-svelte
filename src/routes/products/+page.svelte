2e0df6a157be38cbe9fa9341c37c2f3498ec05f8
Javed Hussain <javedh.dev@gmail.com>
2024-09-21 08:48:10 +0000

    Route Navigation

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,6 +1,13 @@
+<script>
+	export let productID = 100;
+</script>
+
+<a href="/">Home</a>
+
 <h1>Products</h1>
 <ul>
-	<li>Product 1</li>
-	<li>Product 2</li>
-	<li>Product 3</li>
+	<li><a href="/products/1">Product 1</a></li>
+	<li><a href="/products/2">Product 2</a></li>
+	<li><a href="/products/3">Product 3</a></li>
+	<li><a href="/products/{productID}">Product {productID}</a></li>
 </ul>
