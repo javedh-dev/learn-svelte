f32a32b8e771addddf62d30f8f222caaf8be317c
Javed Hussain <javedh.dev@gmail.com>
2024-09-21 03:07:01 +0000

    Nested Dynamic Routing

---
+++ src/routes/products/[productId]/reviews/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+	const productId = $page.params.productId;
+	// const reviews = $page.params.reviews
+</script>
+
+<h1>Reviews for Product : {productId}</h1>
---
+++ src/routes/products/[productId]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+	const productId = $page.params.productId;
+	const reviewId = $page.params.reviewId;
+</script>
+
+<h1>Review: {reviewId} for Product : {productId}</h1>
