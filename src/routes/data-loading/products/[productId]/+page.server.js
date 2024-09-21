534b5cfac9001307d59cc16f2b9172bfbccde927
Javed Hussain <javedh.dev@gmail.com>
2024-09-21 16:41:46 +0000

    Error handling and redirects

--- src/routes/data-loading/products/[productId]/+page.server.js
+++ src/routes/data-loading/products/[productId]/+page.server.js
@@ -1,6 +1,16 @@
+import { error, redirect } from '@sveltejs/kit';
+
 export const load = async ({ fetch, params, url, route }) => {
-	console.log(url);
-	console.log(route);
+	// console.log(url);
+	// console.log(route);
+
+	if (params.productId > 10) {
+		// throw error(404, {
+		// 	message: 'Product Not found!!!',
+		// 	hint: 'Try a different product ID'
+		// });
+		throw redirect(307, '/data-loading/products');
+	}
 
 	const res = await fetch(`http://localhost:4000/products/${params.productId}`);
 	const data = await res.json();
