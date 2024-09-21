bab0afb8748127697058a96aff6cb6bf9e3c1071
Javed Hussain <javedh.dev@gmail.com>
2024-09-21 13:20:50 +0000

    Server load function

--- src/routes/data-loading/products/+page.js
+++ src/routes/data-loading/products/+page.js
@@ -1,14 +1,16 @@
-export const load = async ({ fetch }) => {
-	const res = await fetch('http://localhost:4000/products');
-	const data = await res.json();
-	if (res.ok) {
-		return {
-			products: data
-		};
-	} else {
-		return {
-			status: res.status,
-			error: new Error('Could not load the data')
-		};
-	}
-};
+// export const load = async ({ fetch }) => {
+// 	console.log('Called load functions from +page.js');
+
+// 	const res = await fetch('http://localhost:4000/products');
+// 	const data = await res.json();
+// 	if (res.ok) {
+// 		return {
+// 			products: data
+// 		};
+// 	} else {
+// 		return {
+// 			status: res.status,
+// 			error: new Error('Could not load the data')
+// 		};
+// 	}
+// };
---
+++ src/routes/data-loading/products/+page.server.js
@@ -0,0 +1,16 @@
+export const load = async ({ fetch }) => {
+	console.log('Called load functions from +page.server.js');
+
+	const res = await fetch('http://localhost:4000/products');
+	const data = await res.json();
+	if (res.ok) {
+		return {
+			products: data
+		};
+	} else {
+		return {
+			status: res.status,
+			error: new Error('Could not load the data')
+		};
+	}
+};
