ba761ba78400d53f8124ef5ffdb972097427f560
Javed Hussain <javedh.dev@gmail.com>
2024-09-21 05:24:05 +0000

    Optional Parameter

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from '$app/stores';
+
+	const greeetings = {
+		en: 'Hello',
+		ja: 'こんにちは',
+		es: 'Hola'
+	};
+	const lang = $page.params.lang;
+	const greeting = greeetings[lang != undefined ? lang : 'en'];
+</script>
+
+<h1>{greeting} world!</h1>
