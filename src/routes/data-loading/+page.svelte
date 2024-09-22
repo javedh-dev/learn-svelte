eb736d511fd58ddfadc8716102764c1720c2f94b
Javed Hussain <javedh.dev@gmail.com>
2024-09-22 08:37:03 +0000

    Preload programatically

--- src/routes/data-loading/+page.svelte
+++ src/routes/data-loading/+page.svelte
@@ -1,7 +1,20 @@
-<a href="/data-loading/products/" data-sveltekit-noscroll>Products</a>
+<script>
+	import { goto, preloadData } from '$app/navigation';
+</script>
+
+<a href="/data-loading/products/">Products</a>
+<!-- <a href="/data-loading/products/" data-sveltekit-noscroll>Products</a> -->
 <!-- <a href="/data-loading/products/" data-sveltekit-reload>Products</a> -->
 <!-- <a href="/data-loading/products/" data-sveltekit-preload-data="hover">Products</a> -->
 <!-- <a href="/data-loading/products/" data-sveltekit-preload-data="hover">Products</a> -->
 <!-- <a href="/data-loading/products/" data-sveltekit-preload-code="viewport">Products</a> -->
 <!-- <a href="/data-loading/products/" data-sveltekit-preload-code="viewport">Products</a> -->
 <a href="/data-loading/stocks/">Stocks</a>
+
+<button
+	on:focus={async () => await preloadData('/data-loading/products/')}
+	on:mouseover={async () => await preloadData('/data-loading/products/')}
+	on:click={() => goto('/data-loading/products/')}
+>
+	Go to products
+</button>
