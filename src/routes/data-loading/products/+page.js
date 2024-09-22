caaa34a20a11df8b8fce993de5267bfb4bb6def7
Javed Hussain <javedh.dev@gmail.com>
2024-09-22 11:52:53 +0000

    CSR SSR and Prerender

--- src/routes/data-loading/products/+page.js
+++ src/routes/data-loading/products/+page.js
@@ -1,7 +1,14 @@
 export const load = async ({ fetch }) => {
+	console.log('Calling page.server.js');
+
 	const res = await fetch('http://192.168.1.121:4000/products');
 	const data = await res.json();
 	return {
 		products: data
 	};
 };
+
+// export const ssr = false;
+// export const csr = true;
+
+// export const prerender = true;
