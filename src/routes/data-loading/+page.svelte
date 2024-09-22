dcaa0ece9aee552d69cc9da2920970968d8742d8
Javed Hussain <javedh.dev@gmail.com>
2024-09-22 08:26:29 +0000

    Preloading code

--- src/routes/data-loading/+page.svelte
+++ src/routes/data-loading/+page.svelte
@@ -1,2 +1,4 @@
-<a href="/data-loading/products/" data-sveltekit-preload-data="off">Products</a>
+<a href="/data-loading/products/" data-sveltekit-preload-data="hover">Products</a>
+<!-- <a href="/data-loading/products/" data-sveltekit-preload-code="viewport">Products</a> -->
+<!-- <a href="/data-loading/products/" data-sveltekit-preload-code="viewport">Products</a> -->
 <a href="/data-loading/stocks/">Stocks</a>
