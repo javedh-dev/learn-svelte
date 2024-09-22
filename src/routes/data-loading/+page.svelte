8ae7f9c0c12caaee920f1aa5fb58255f40d411ea
Javed Hussain <javedh.dev@gmail.com>
2024-09-22 07:53:06 +0000

    Promise Unwrapping

--- src/routes/data-loading/+page.svelte
+++ src/routes/data-loading/+page.svelte
@@ -1,1 +1,2 @@
 <a href="/data-loading/products/">Products</a>
+<a href="/data-loading/stocks/">Stocks</a>
---
+++ src/routes/data-loading/stocks/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let data;
+</script>
+
+<h1>The stock exchange</h1>
+<p>
+	The stock exchange is a place where shares of publicly traded companies are bought and sold. It is
+	a marketplace where investors can buy and sell shares of companies they believe will perform well
+	in the future. The stock exchange provides a platform for companies to raise capital by issuing
+	shares to the public, and for investors to make money by buying and selling those shares.
+</p>
+
+<ul>
+	<li>Top Active : {data.topActive.symbol}</li>
+	<li>Top Gaining : {data.topGaining.symbol}</li>
+	<li>Top Losing : {data.topLosing.symbol}</li>
+</ul>
